044df8e394d7c4409d14e6b06fabcfc00f6620be
Michael Mendelsohn <mjm111@duke.edu>
2017-02-28 00:43:11 +0000

    Fixes User Zipcode Query

--- Database/db_queries.sql
+++ Database/db_queries.sql
@@ -2,7 +2,8 @@ SELECT Restaurant.name
 FROM Restaurant
 WHERE Restaurant.price_range = 4;
 
-SELECT R.name
+--Finds name of restaurants in the same zipcode as each user
+SELECT R.name, U.username
 FROM Restaurant AS R, RestaurantAt AS RA, Users AS U, LivesAt AS L
 WHERE R.rest_id = RA.rest_id AND RA.zip_code = L.zip_code AND U.username = L.username;
 
