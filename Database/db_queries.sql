ecb83c3356eaa44400e0ede6b6ee72977874c63a
mmungekar <mmungekar7@yahoo.com>
2017-02-27 22:18:24 -0500

    Update db_queries.sql

--- Database/db_queries.sql
+++ Database/db_queries.sql
@@ -1,7 +1,3 @@
-SELECT Restaurant.name
-FROM Restaurant
-WHERE Restaurant.price_range = 4;
-
 --Finds name of restaurants in the same zipcode as each user
 SELECT R.name, U.username
 FROM Restaurant AS R, RestaurantAt AS RA, Users AS U, LivesAt AS L
@@ -9,13 +5,23 @@ WHERE R.rest_id = RA.rest_id AND RA.zip_code = L.zip_code AND U.username = L.use
 
 -- Retrieving password of a username, 'uname'
 SELECT password FROM Users u WHERE u.username = 'uname';
+
 -- Select only the names of restaurants that don't serve alcohol
 SELECT name FROM Restaurant r WHERE r.alcohol = 'f';
+
 -- Select only the names of restaurants with a price range of $10-20 and deliver
 SELECT name FROM Restaurant r WHERE r.price_range = 3;
--- Select only the names of restaurants that serve a selected food, 'peaches'
-SELECT name from Restaurant r, Serves s, Food f WHERE f.food_id = s.food_id AND r.rest_id = s.rest_id AND f.item = 'peaches';
--- Select only the names of restaurants at a particular ZIP code, 12345
-SELECT name from Restaurant r1, RestaurantAt r2 WHERE r1.rest_id = r2.rest_id AND r2.zip_code = 12345;
+
+-- Select only the names of restaurants that serve a selected food, 'Scallops'
+SELECT name from Restaurant r, Serves s, Food f WHERE f.food_id = s.food_id AND r.rest_id = s.rest_id AND f.item = 'Scallops';
+
 -- Get the number of users at a particular ZIP code, 12345
 SELECT COUNT(DISTINCT username) FROM LivesAt la WHERE la.zip_code = 12345;
+
+---Selects all restaurants that a pair of friends have swiped the same way on
+SELECT Friends.username1,Friends.username2, u1.rest_id
+FROM Friends, Userswipe u1, Userswipe u2
+WHERE Friends.username1 = u1.username
+AND Friends.username2 = u2.username
+AND u1.rest_id = u2.rest_id
+AND u1.likes = u2.likes;
