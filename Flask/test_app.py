5871ce1cbef44b0fae071f8c546e39003fc5ec78
Michael Mendelsohn <MJMendelsohn@users.noreply.github.com>
2017-03-20 18:16:56 -0400

    Fixes Sample Flask Server

--- Flask/test_app.py
+++ Flask/test_app.py
@@ -7,6 +7,4 @@ def hello_world():
     #return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+    app.run()
