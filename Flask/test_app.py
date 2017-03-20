5e7463022c71c68eda24ea390535384643263d75
Michael Mendelsohn <MJMendelsohn@users.noreply.github.com>
2017-03-20 18:19:28 -0400

    Fixes Sample Page

--- Flask/test_app.py
+++ Flask/test_app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
-    #return app.send_static_file('index.html')
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run()
