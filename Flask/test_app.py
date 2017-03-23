0917165e0607ead864fdeaadc878a17d77d7be49
Michael Mendelsohn <MJMendelsohn@users.noreply.github.com>
2017-03-23 14:57:35 -0400

    Adds Sample POST Code

--- Flask/test_app.py
+++ Flask/test_app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
+
+@app.route('/survey', methods = ['POST'])
+def handle_servey():
+    username = request.form['username']
+    return render_template('survey_resp.html', username=username)
+
 
 if __name__ == '__main__':
     app.run()
