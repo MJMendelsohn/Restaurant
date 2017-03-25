2372474954d9f6c12f961ba25a9195c8d158e037
Aditya Paruchuri <ap376@duke.edu>
2017-03-25 17:49:52 -0400

    updated queries

--- Database/db_queries.sql
+++ Database/db_queries.sql
@@ -10,10 +10,7 @@ SELECT password FROM Users u WHERE u.username = 'uname';
 SELECT name FROM Restaurant r WHERE r.alcohol = 'f';
 
 -- Select only the names of restaurants with a price range of $10-20 and deliver
-SELECT name FROM Restaurant r WHERE r.price_range = 3;
-
--- Select only the names of restaurants that serve a selected food, 'Scallops'
-SELECT name from Restaurant r, Serves s, Food f WHERE f.food_id = s.food_id AND r.rest_id = s.rest_id AND f.item = 'Scallops';
+SELECT name FROM Restaurant r WHERE r.price_range = 3 AND r.delivers = 't';
 
 -- Get the number of users at a particular ZIP code, 12345
 SELECT COUNT(DISTINCT username) FROM LivesAt la WHERE la.zip_code = 12345;
@@ -25,3 +22,13 @@ WHERE Friends.username1 = u1.username
 AND Friends.username2 = u2.username
 AND u1.rest_id = u2.rest_id
 AND u1.likes = u2.likes;
+
+-- Select all restaurants with 4-star ratings or more
+SELECT name FROM Restaurant r where r.rating >= 4.0;
+
+-- Select all restaurants that users have dined at
+SELECT name from Restaurant r, DinesAt d WHERE d.dining_date != 'NULL' AND d.rest_id = r.rest_id;
+
+-- Select all restaurants based on survey responses (zip code [12345], attire [formal], price range [3], alcohol [true], and sit_down [true])
+SELECT name FROM Restaurant r, DinesAt d WHERE r.zip_code = 12345 AND r.attire = 'formal' AND r.price_range = 3 AND r.alcohol = 't' AND r.sit_down = 't' AND d.dining_date != 'NULL' AND r.rest_id = d.rest_id;
+
