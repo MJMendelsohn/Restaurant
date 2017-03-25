9a1bc15e6506e87f4558801d61a917149c186ca3
Michael Mendelsohn <MJMendelsohn@users.noreply.github.com>
2017-03-25 12:50:26 -0400

    Collects and Returns Survey Responses

--- Flask/test_app.py
+++ Flask/test_app.py
@@ -6,13 +6,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('analysisprogress.html')
+    #return render_template('index.html')
 
 @app.route('/survey', methods = ['POST'])
 def handle_servey():
     username = request.form['username']
-    return render_template('survey_resp.html', username=username)
-
+    distance = request.form['distance']
+    party_size = request.form['partysize']
+    formal = request.form['formal']
+    alcohol = request.form['alcohol']
+    min_price = request.form['minprice']
+    max_price = request.form['maxprice']
+    delivery = request.form['delivery']
+    new_old = request.form['newOld']
+    return render_template('survey_resp.html', username=username,
+            distance=distance, party_size=party_size, formal=formal,
+            alcohol=alcohol, min_price=min_price, max_price=max_price,
+            delivery=delivery, new_old=new_old)
 
 if __name__ == '__main__':
     app.run()
