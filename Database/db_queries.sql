cc6679c4020d5648cf2795551074d0f936a0fb9b
Aditya Paruchuri <ap376@duke.edu>
2017-03-27 18:29:42 -0400

    updated last query

--- Database/db_queries.sql
+++ Database/db_queries.sql
@@ -30,5 +30,5 @@ SELECT name FROM Restaurant r where r.rating >= 4.0;
 SELECT name from Restaurant r, DinesAt d WHERE d.dining_date != 'NULL' AND d.rest_id = r.rest_id;
 
 -- Select all restaurants based on survey responses (zip code [12345], attire [formal], price range [3], alcohol [true], and sit_down [true])
-SELECT name FROM Restaurant r, DinesAt d WHERE r.zip_code = 12345 AND r.attire = 'formal' AND r.price_range = 3 AND r.alcohol = 't' AND r.sit_down = 't' AND d.dining_date != 'NULL' AND r.rest_id = d.rest_id;
+SELECT name FROM Restaurant r, DinesAt d, RestaurantAt ra WHERE ra.zip_code = 12345 AND r.attire = 'formal' AND r.price_range = 3 AND r.alcohol = 't' AND r.sit_down = 't' AND d.dining_date != 'NULL' AND r.rest_id = d.rest_id AND ra.rest_id = d.rest_id AND ra.rest_id = r.rest_id;
 
