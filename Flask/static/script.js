a92012d544e988fadcdd4beca761ac56e97ee1a9
Michael Mendelsohn <MJMendelsohn@users.noreply.github.com>
2017-04-24 23:01:16 -0400

    Adds Swiping Page

--- Flask/static/script.js
+++ Flask/static/script.js
@@ -1,36 +1,42 @@
-var images = ["delivery.jpg", "fast-food.jpg", "fancy.jpg"];
-var desc = ["Do you want a restaurant that delivers?", "Do you want to go to a fast-food restaurant?", "Do you want a fancy restaurant?"];
-var userSelected = [];
-var j = 0;
+function createSwipe(survey_results) {
+    var images = ["delivery.jpg", "fast-food.jpg", "fancy.jpg"];
+//    var desc = ["Do you want a restaurant that delivers?", "Do you want to go to a fast-food restaurant?", "Do you want a fancy restaurant?"];
+    var desc = survey_results;
+    var userSelected = [];
+    var j = 1;
 
-for (var i = 0; i < images.length; i++) {
-  userSelected.push(0);
+    for (var i = 0; i < images.length; i++) {
+      userSelected.push(0);
+    }
+
+
+    var left = document.getElementById("left-arrow");
+
+    document.getElementById("currImage").src = "static/"+images[0];
+    document.getElementById("caption").innerHTML = desc[0];
+
+    left.addEventListener("click", function() {
+      var curImage = document.getElementById("currImage").src;
+      userSelected[j] = 0;
+
+      if (j < desc.length - 1) {
+        j = j + 1;
+      }
+      //alert("Hello");
+      //document.getElementById("currImage").src = images[j];
+      document.getElementById("caption").innerHTML = desc[j];
+    });
+
+    var right = document.getElementById("right-arrow");
+
+    right.addEventListener("click", function() {
+      var curImage = document.getElementById("currImage").src;
+      userSelected[j] = 1;
+
+      if (j < desc.length - 1) {
+        j = j + 1;
+      }
+      //document.getElementById("currImage").src = images[j];
+      document.getElementById("caption").innerHTML = desc[j];
+    });
 }
-
-
-var left = document.getElementById("left-arrow");
-
-left.addEventListener("click", function() {
-  var curImage = document.getElementById("currImage").src;
-  userSelected[j] = 0;
-
-  if (j < images.length - 1) {
-    j = j + 1;
-  }
-  //alert("Hello");
-  document.getElementById("currImage").src = images[j];
-  document.getElementById("caption").innerHTML = desc[j];
-});
-
-var right = document.getElementById("right-arrow");
-
-right.addEventListener("click", function() {
-  var curImage = document.getElementById("currImage").src;
-  userSelected[j] = 1;
-
-  if (j < images.length - 1) {
-    j = j + 1;
-  }
-  document.getElementById("currImage").src = images[j];
-  document.getElementById("caption").innerHTML = desc[j];
-});
