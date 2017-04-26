0f69cfc74aeca232076a9f6d5e1d6e4dbef92a22
ra44139 <ra44139@gmail.com>
2017-04-26 17:00:41 -0400

    added bash file to automate setup -- hopefully enough

--- Flask/static/survey.js
+++ Flask/static/survey.js
@@ -3,7 +3,6 @@ $( document ).ready(function() {
     $("#submit").on("click", function () {
         $.post("/survey", $("#form").serialize(),
                 function(dataFromServer) {
-                  if(dataFromServer.length=)
                   $("#main").load("/swipe", function () {
                       createSwipe(dataFromServer);
                   });
